46a56ade58f520b619870e6d59b238a15287de30
Robin Schaaf <robinmschaaf@gmail.com>
2012-09-14 11:15:16 -0400

    added back button

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -3,6 +3,9 @@
 
          $.mobile.loadingMessageTheme = 'c';
          $.mobile.loadingMessageTextVisible = false;
+         $.mobile.page.prototype.options.backBtnTheme = "b";
+         $.mobile.page.prototype.options.backBtnText = "back";
+         $.mobile.page.prototype.options.mini = "true";
 
         }).live('[data-role="page"]', 'click', function () {
              if (!$(this).hasClass( "mobileDBLink" ))
@@ -13,8 +16,5 @@
 
 
 
-     $(".mobileDBLink").click(function(){
-alert("mobiledb")
-     });
 
 

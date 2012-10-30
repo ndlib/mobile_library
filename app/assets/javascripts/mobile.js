3e644ccc3e0c18964ce44324dfbd2e17e4f57147
Robin Schaaf <robinmschaaf@gmail.com>
2012-10-30 15:39:30 -0400

    continue work on cross domain transfers

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -5,6 +5,7 @@
          $.mobile.loadingMessageTextVisible = false;
          $.mobile.allowCrossDomainPages = true;
          $.support.cors = true;
+         $.mobile.page.prototype.options.addBackBtn = "true";
          $.mobile.page.prototype.options.backBtnTheme = "b";
          $.mobile.page.prototype.options.backBtnText = "back";
 

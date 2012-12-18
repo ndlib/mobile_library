14127885dd33d17f5c21e99e252228a0e8cd27bc
Robin Schaaf <robinmschaaf@gmail.com>
2012-12-18 09:41:55 -0500

    deploy script fix

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ group :assets do
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
   gem 'uglifier', '>= 1.0.3'
+
+  gem 'therubyracer', :platform => :ruby
 end
 
 gem 'jquery-rails'
@@ -36,6 +38,6 @@ gem 'jquery-rails'
 gem 'nokogiri'
 gem "dynamic_form", "~> 1.1.4"
 gem 'execjs'
-gem 'therubyracer', :platform => :ruby
+
 gem 'savon'
 gem 'httpi'
--- config/deploy.rb
+++ config/deploy.rb
@@ -45,7 +45,7 @@ task :pre_production do
 
   set :rails_env, 'pre_production'
   set :deploy_to, "/shared/ruby_pprd/data/app_home/#{application}"
-  set :ruby_bin,  '/shared/ruby_prod/ruby/bin'
+  set :ruby_bin,  '/shared/ruby_pprd/ruby/bin'
   set :ruby,      File.join(ruby_bin, 'ruby')
   set :bundler,   File.join(ruby_bin, 'bundle')
   set :rake,      File.join(shared_path, 'vendor/bundle/ruby/1.8/bin/rake')

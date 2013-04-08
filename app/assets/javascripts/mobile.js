de11bbb33e4fc33b8fa0764e8439f35e1b13e183
Robin Schaaf <robinmschaaf@gmail.com>
2013-04-08 11:46:58 -0400

    updates for hours now that it's in prod

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -22,16 +22,14 @@
              }
 
          });
-
-
-
+         
          $('.popupLink').click(function() {
              $(".popupMap").popup("open")
              return false;
           });
 
 
-         getServiceHours({ architecture_library: '#architecture_hours' });
+        getServiceHours({hesburgh: '#hesburgh_destination',circulation: '#circulation_destination',computer_lab: '#computer_lab_destination',lower_level_service_desk: '#lower_level_service_desk_destination',medieval: '#medieval_destination',music_and_media: '#music_and_media_destination',reference: '#reference_destination',special_collection: '#special_collection_destination',archives: '#archives_destination',architecture_library: '#architecture_library_destination',bic: '#bic_destination',chem_phys_library: '#chem_phys_library_destination',engineering_library: '#engineering_library_destination',kellogg_library: '#kellogg_library_destination',math_library: '#math_library_destination',radiation: '#radiation_destination',visual_resources: '#visual_resources_destination' });
 
 
      });

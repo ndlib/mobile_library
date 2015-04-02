52b0b3e583d0e5629fffa9dd53ff75a4a223f7e4
Robert Fox <rfox2@nd.edu>
2015-04-02 16:06:26 -0400

    Update capistrano to 3.3.5

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '~> 3.2.3'
-gem 'capistrano'
+gem 'capistrano', '3.3.5'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,63 +1,46 @@
-# Set the name of the application.  This is used to determine directory paths and domains
-set :application, 'mobile_library'
+# config valid only for current version of Capistrano
+lock '3.3.5'
 
-#############################################################
-#  Application settings
-#############################################################
-
-# Defaults are set in lib/hesburgh_infrastructure/capistrano/common.rb
-
-# Repository defaults to "git@git.library.nd.edu:#{application}"
-# set :repository, "git@git.library.nd.edu:myrepository"
-
-# Define symlinks for files or directories that need to persist between deploys.
-# /log, /vendor/bundle, and /config/database.yml are automatically symlinked
-set :application_symlinks, [
-  "/config/initializers/refworks_admin.rb"
-]
-
-#############################################################
-#  Environment settings
-#############################################################
-
+set :application, 'mobile'
 set :repo_url, 'https://github.com/ndlib/mobile_library.git'
-# Set an environment variable to deploy from a branch other than master
-# branch=beta cap staging deploy
-set(:branch) {
-  name = ENV['branch'] ? ENV['branch'] : 'master'
 
-  if name == 'master'
-    set :git_shallow_clone, 1
+set :log_level, :info
+
+# Default branch is :master
+if fetch(:stage).to_s == 'production'
+  ask :branch, 'master'
+else
+  ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+end
+
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, '/home/app/mobile'
+
+# Default value for :linked_files is []
+set :linked_files, %w{config/database.yml}
+
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/system vendor/bundle}
+
+set :default_env, { path: "/opt/ruby/current/bin:$PATH" }
+
+namespace :deploy do
+
+  Rake::Task["migrate"].clear_actions
+  task :migrate do ; end
+
+  desc 'Restart application'
+
+  task :restart do
+    on roles(:web), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
   end
 
-  puts "Deploying to branch #{name}"
-  name
-}
+  after :publishing, :restart
 
-set :scm, 'git'
-set :scm_command, '/usr/bin/git'
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    end
+  end
 
-set :user, 'app'
-set :ruby_bin, "/opt/ruby/current/bin"
-
-desc "Setup for the Pre-Production environment"
-task :pre_production do
-  # Customize pre_production configuration
-  set :deploy_to, "/home/app/mobile"
-  set :domain, "mobilepprd-vm.library.nd.edu"
 end
-
-desc "Setup for the production environment"
-task :production do
-  # Customize production configuration
-  set :deploy_to, "/home/app/mobile"
-  set :domain, "mobileprod-vm.library.nd.edu"
-end
-
-#############################################################
-#  Additional callbacks and tasks
-#############################################################
-
-# Define any addional tasks or callbacks here
-
-

f77832ecd3be49b46100faae637853aa5f2f270f
Robert Fox <rfox2@nd.edu>
2016-04-21 10:53:36 -0400

    Fix for jQuery mobile and inline javascript

--- app/controllers/renalerts_controller.rb
+++ app/controllers/renalerts_controller.rb
@@ -1,13 +1,16 @@
 class RenalertsController < ApplicationController
   require "rubygems"
   require "open-uri"
-  require "nokogiri"
 
   def index
     @renalerts = open(
       "http://us9.campaign-archive2.com/generate-js/?u=3e737e755493a925f88711b17&fid=8753&show=3",
       "User-Agent" => "Ruby-OpenURI",
       "Referer" => "http://m.library.nd.edu/"
-    ).read
+    ).read.to_s
+    @renalerts.gsub!(/document\.write\(\"/, "")
+    @renalerts.gsub!(/\"\)/, "")
+    @renalerts.delete! "\\"
+    @renalerts.delete! ";"
   end
 end

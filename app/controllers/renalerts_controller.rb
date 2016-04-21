7d7576b9e49fa49a6ad956cb1dde0b397bb87869
Robert Fox <rfox2@nd.edu>
2016-04-21 09:54:58 -0400

    Fix for HTTPS connection issue

--- app/controllers/renalerts_controller.rb
+++ app/controllers/renalerts_controller.rb
@@ -1,6 +1,13 @@
 class RenalertsController < ApplicationController
+  require "rubygems"
+  require "open-uri"
+  require "nokogiri"
 
   def index
+    @renalerts = open(
+      "http://us9.campaign-archive2.com/generate-js/?u=3e737e755493a925f88711b17&fid=8753&show=3",
+      "User-Agent" => "Ruby-OpenURI",
+      "Referer" => "http://m.library.nd.edu/"
+    ).read
   end
-
 end
